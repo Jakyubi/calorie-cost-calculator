7b9e975f4a5a0cc9eb1863872681893f37071151
Jakyubi <frostwth.xd@gmail.com>
2026-04-13 18:57:01 +0200

    Refactor insert.php with prepared statements

--- insert.php
+++ insert.php
@@ -1,17 +1,22 @@
 <?php
 require('db_connect.php');
 $name = $_POST['name'];
-$kcal = $_POST['kcal'];
-$cost = $_POST['cost'];
-$weight = $_POST['weight'];
-$calCost = $kcal/$cost;
-$calCost = round($calCost, 3);
-$calWeight = $kcal/$weight;
-$calWeight = round($calWeight, 3);
-$wholeCal = $calWeight * $weight;
+$kcal_per_100g = (int)$_POST['kcal_per_100g'];
+$price = (float)$_POST['price'];
+$weight_g = (float)$_POST['weight_g'];
+$kcal_per_g = $kcal_per_100g/100;
+$total_kcal = $kcal_per_g * $weight_g;
+$kcal_per_pln = $total_kcal / $price;
 
+$stmt = $conn->prepare("INSERT INTO products (name, kcal_per_100g, price, weight_g, kcal_per_g, total_kcal, kcal_per_pln) VALUES (?,?,?,?,?,?,?)");
+$stmt->bind_param("siddddd", $name, $kcal_per_100g, $price, $weight_g, $kcal_per_g, $total_kcal, $kcal_per_pln);
 
-$sql = "INSERT INTO main  VALUES ('NULL', '$name', '$kcal', '$cost', '$weight', $calCost, $calWeight, $wholeCal)";
-mysqli_query($conn, $sql);
-header('Location: index.php');
+if($stmt->execute()){
+    $stmt->close();
+    header('Location: index.php');
+    exit();
+}else{
+    echo "Error while inserting to database";
+}
+
 ?>
